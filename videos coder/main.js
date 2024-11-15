f30a92e1cfeacf2e21451fec322e59f1b5a2221d
Ezequiel Amurua <ezequielamurua@hotmail.com>
2024-11-15 00:38:29 -0300

    html

---
+++ videos coder/main.js
@@ -0,0 +1,17 @@
+// Videos Coder - Sugar Syntax📚
+const condition = false;
+let result = null;
+
+// if (condition) {                 Estructura If/Else📌
+//     result = "correct"
+// }else{
+//     result = "incorrect"
+// }
+// console.log(`This is ${result}`)
+
+// console.log(`This is ${condition ? "correct" : "incorrect"}`) O.Ternario📌
+
+
+// console.log(`This is ${condition ? "correct" : "incorrect"}`)   O.Ternario📌
+
+
